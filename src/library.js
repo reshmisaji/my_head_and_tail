a32a2638fc5138b9bb50d3ab3c4b323ee3c06668
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-13 20:23:03 +0530

    introduced function getContents()

--- src/library.js
+++ src/library.js
@@ -1,7 +1,13 @@
-const parseInput = function(userInput){
-    let parsedInput = {bytes : undefined, lines : 10, fileNames : []};
-    parsedInput.fileNames = parsedInput.fileNames.concat(userInput);
-    return parsedInput;
-}
+const fs = require("fs");
 
-module.exports = {parseInput};
+const parseInput = function(userInput) {
+  let parsedInput = { bytes: undefined, lines: 10, fileNames: [] };
+  parsedInput.fileNames = parsedInput.fileNames.concat(userInput);
+  return parsedInput;
+};
+
+const getContents = function(fileName) {
+  return fs.readFileSync(fileName, "utf-8");
+};
+
+module.exports = { parseInput, getContents };
--- test/libraryTest.js
+++ test/libraryTest.js
@@ -3,14 +3,23 @@ const { parseInput } = require("../src/library.js");
 
 describe("parseInput", function() {
   describe("Passing only the file names", function() {
-    it('Should return an object containing bytes as undefined lines as 10 and fileNames as the given file name in an array when given a file', function() {
+    it("should return  an object with bytes as undefined lines as 10 and fileNames as an empty array when we dont give filename", function() {
+      deepEqual(parseInput([""]), {
+        bytes: undefined,
+        lines: 10,
+        fileNames: [""]
+      });
+    });
+
+    it("Should return an object containing bytes as undefined lines as 10 and fileNames as the given file name in an array when given a file", function() {
       deepEqual(parseInput(["example1.js"]), {
         bytes: undefined,
         lines: 10,
         fileNames: ["example1.js"]
       });
     });
-    it('Should return an object containing bytes as undefined lines as 10 and fileNames as the given file names in an array when given more than file', function() {
+
+    it("Should return an object containing bytes as undefined lines as 10 and fileNames as the given file names in an array when given more than file", function() {
       deepEqual(parseInput(["example1.js", "example2.js"]), {
         bytes: undefined,
         lines: 10,
