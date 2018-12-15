79ea513a0440e7ef3a19109fed9a6605a6767d78
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-15 09:22:49 +0530

    modify main() to process multiple files

--- head.js
+++ head.js
@@ -1,11 +1,14 @@
-const {parseInput,getContents} = require('./src/library.js');
-const fs = require('fs');
+const { parseInput, getContents } = require("./src/library.js");
+const fs = require("fs");
 
 const main = function(parameters) {
   let input = parseInput(parameters);
-  let contents = getContents(fs,input.fileNames[0],input.lines);
-  console.log(contents);
-}
+  let contents = [];
+
+  for (let index = 0; index < parameters.length; index++) {
+    contents.push(getContents(fs, input.fileNames[index], input.lines));
+  }
+  contents.map(x=>console.log(x));
+};
 
 main(process.argv.slice(2));
-
